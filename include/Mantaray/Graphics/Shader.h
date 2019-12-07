f197a9597b1cfbdd5aa1070512e283e4e7636561
nicohirsau <preussens.nico@gmail.com>
2019-12-07 15:03:27 +0100

    Shader can now be used as an object

--- include/Mantaray/Graphics/Shader.h
+++ include/Mantaray/Graphics/Shader.h
@@ -1,5 +1,9 @@
-namespace MR {
+#include <string>
+#include <unordered_map>
 
+#include "Mantaray/Core/Vector.h"
+
+namespace MR {
 class Shader {
     public:
         enum ShaderType {
@@ -7,7 +11,23 @@ class Shader {
             FRAGMENT_SHADER
         };
 
-        static unsigned int compileShader(Shader::ShaderType st, const char* source);
+        Shader(const char* vertexShaderSource, const char* fragmentShaderSource);
+        Shader(std::string vertexShaderPath, std::string fragmentShaderPath);
+        ~Shader();
+
+        void activate();
+        void setUniformFloat(std::string uniformName, float value);
+        void setUniformVector2f(std::string uniformName, Vector2f value);
+        void setUniformVector3f(std::string uniformName, Vector3f value);
+
+        static unsigned int compileShader(Shader::ShaderType shaderType, const char* source);
         static unsigned int linkShader(unsigned int vertexShader, unsigned int fragmentShader);
+
+    private:
+        int getUniformLocation(std::string uniformName);
+
+    private:
+        unsigned int m_FragmentShaderID, m_VertexShaderID, m_ShaderProgramID;
+        std::unordered_map<std::string, int> m_Uniforms = std::unordered_map<std::string, int>();
 };
 }
--- src/Mantaray/Shader.cpp
+++ src/Mantaray/Shader.cpp
@@ -1,23 +1,84 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
-#include "Mantaray/Graphics/Shader.h"
+#include "Mantaray/Core/FileSystem.h"
 #include "Mantaray/Core/Logger.h"
+#include "Mantaray/Graphics/Shader.h"
 
 using namespace MR;
 
-unsigned int Shader::compileShader(Shader::ShaderType st, const char* source) {
-    unsigned int shaderID;
-    std::string shaderType;
+Shader::Shader(const char* vertexShaderSource, const char* fragmentShaderSource) {
+    m_VertexShaderID = compileShader(VERTEX_SHADER, vertexShaderSource);
+    m_FragmentShaderID = compileShader(FRAGMENT_SHADER, fragmentShaderSource);
+    m_ShaderProgramID = linkShader(m_VertexShaderID, m_FragmentShaderID);
+}
 
-    switch (st)
+Shader::Shader(std::string vertexShaderPath, std::string fragmentShaderPath) {
+    std::string vertexShaderContent;
+    FileSystem::readFile(vertexShaderPath, vertexShaderContent);
+    const char* vertexShaderSource = vertexShaderContent.c_str();
+    
+    std::string fragmentShaderContent;
+    FileSystem::readFile(fragmentShaderPath, fragmentShaderContent);
+    const char* fragmentShaderSource = fragmentShaderContent.c_str();
+
+    m_VertexShaderID = compileShader(VERTEX_SHADER, vertexShaderSource);
+    m_FragmentShaderID = compileShader(FRAGMENT_SHADER, fragmentShaderSource);
+    m_ShaderProgramID = linkShader(m_VertexShaderID, m_FragmentShaderID);
+}
+
+Shader::~Shader() {
+    glDeleteShader(m_VertexShaderID);
+    glDeleteShader(m_FragmentShaderID);
+    glDeleteProgram(m_ShaderProgramID);
+}
+
+void Shader::activate() {
+    glUseProgram(m_ShaderProgramID);
+}
+
+void Shader::setUniformFloat(std::string uniformName, float value) {
+    int uniformLocation = getUniformLocation(uniformName);
+    glUniform1f(uniformLocation, value);
+}
+
+void Shader::setUniformVector2f(std::string uniformName, Vector2f value) {
+    int uniformLocation = getUniformLocation(uniformName);
+    glUniform2f(uniformLocation, value.x, value.y);    
+}
+
+void Shader::setUniformVector3f(std::string uniformName, Vector3f value) {
+    int uniformLocation = getUniformLocation(uniformName);
+    glUniform3f(uniformLocation, value.x, value.y, value.z);    
+}
+
+int Shader::getUniformLocation(std::string uniformName) {
+    int uniformLocation = -1;
+
+    std::unordered_map<std::string, int>::iterator it;
+    it = m_Uniforms.find(uniformName);
+
+    if (it == m_Uniforms.end()) {
+        uniformLocation = glGetUniformLocation(m_ShaderProgramID, uniformName.c_str());
+        if (uniformLocation == -1) {
+            Logger::Log("Shader", "Cannot find uniform location for: " + uniformName, Logger::LOG_WARNING);
+        }
+        m_Uniforms[uniformName] = uniformLocation;
+    } else {
+        uniformLocation = it->second;
+    }
+    return uniformLocation;
+}
+
+unsigned int Shader::compileShader(Shader::ShaderType shaderType, const char* source) {
+    unsigned int shaderID;
+
+    switch (shaderType)
     {
     case Shader::VERTEX_SHADER:
-        shaderType = "VERTEX";
         shaderID = glCreateShader(GL_VERTEX_SHADER);
         break;
     case Shader::FRAGMENT_SHADER:
-        shaderType = "FRAGMENT";
         shaderID = glCreateShader(GL_FRAGMENT_SHADER);
         break;
     
@@ -33,7 +94,8 @@ unsigned int Shader::compileShader(Shader::ShaderType st, const char* source) {
     glGetShaderiv(shaderID, GL_COMPILE_STATUS, &success);
     if(!success) {
         glGetShaderInfoLog(shaderID, 512, NULL, infoLog);
-        std::string message = shaderType + "::COMPILING_SHADER_FAILED\n";
+        std::string s_ShaderType = (shaderType == VERTEX_SHADER) ? "VERTEX" : "FRAGMENT";
+        std::string message = s_ShaderType + "::COMPILING_SHADER_FAILED\n";
         message.append(infoLog);
         Logger::Log("ShaderCompiler", message, Logger::LOG_ERROR);
         return -1;
