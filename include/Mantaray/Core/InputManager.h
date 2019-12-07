f114078d1494fd60d8ca8b0395daf632192054d1
nicohirsau <preussens.nico@gmail.com>
2019-12-07 15:01:33 +0100

    Implementation of Inputmananger

--- include/Mantaray/Core/InputManager.h
+++ include/Mantaray/Core/InputManager.h
@@ -1,22 +1,29 @@
 #include <map>
+#include <vector>
 
 #include <GLFW/glfw3.h>
 
 #include "Mantaray/Core/Vector.h"
 
 namespace MR {
-class InputManager {
+class InputManager{
     public:
         static void setWindowHandle(GLFWwindow* windowHandle);
         static GLFWwindow* getWindowHandle();
 
+        static void update(float deltaTime);
+
+        static void addKeyToWatch(int keyCode);
+
         static bool getKey(int keyCode);
         static bool getKeyDown(int keyCode);
+        static bool getKeyUp(int keyCode);
         static void getMousePosition(Vector2d &mousePos);
 
     private:
         static GLFWwindow* m_WindowHandle;
 
+        static std::vector<int> m_WatchedKeys;
         static std::map<int, float> m_WatchedKeysElapsedTime;
         static std::map<int, bool> m_WatchedKeysDown;
         static std::map<int, bool> m_WatchedKeysUp;
--- src/Mantaray/InputManager.cpp
+++ src/Mantaray/InputManager.cpp
@@ -5,6 +5,11 @@ using namespace MR;
 
 GLFWwindow* InputManager::m_WindowHandle = nullptr;
 
+std::vector<int> InputManager::m_WatchedKeys = std::vector<int>();
+std::map<int, float> InputManager::m_WatchedKeysElapsedTime = std::map<int, float>();
+std::map<int, bool> InputManager::m_WatchedKeysDown = std::map<int, bool>();
+std::map<int, bool> InputManager::m_WatchedKeysUp = std::map<int, bool>();
+
 void InputManager::setWindowHandle(GLFWwindow* windowHandle) {
     InputManager::m_WindowHandle = windowHandle;
 }
@@ -16,12 +21,43 @@ GLFWwindow* InputManager::getWindowHandle() {
     return InputManager::m_WindowHandle;
 }
 
+void InputManager::update(float deltaTime) {
+    GLFWwindow* windowHandle = InputManager::getWindowHandle();
+    if (windowHandle == nullptr)
+        return;
+    
+    for (unsigned int i = 0; i < InputManager::m_WatchedKeys.size(); i++) {
+        int keyCode = InputManager::m_WatchedKeys[i];
+        if (InputManager::getKey(keyCode)) {
+            InputManager::m_WatchedKeysDown[keyCode] = (InputManager::m_WatchedKeysElapsedTime[keyCode] > 0);
+            InputManager::m_WatchedKeysElapsedTime[keyCode] = 0;
+            InputManager::m_WatchedKeysUp[keyCode] = false;
+        } else {
+            InputManager::m_WatchedKeysUp[keyCode] = (InputManager::m_WatchedKeysElapsedTime[keyCode] == 0);
+            InputManager::m_WatchedKeysElapsedTime[keyCode] += deltaTime;
+            InputManager::m_WatchedKeysDown[keyCode] = false;
+        }
+    }
+}
+
+void InputManager::addKeyToWatch(int keyCode) {
+    InputManager::m_WatchedKeys.push_back(keyCode);
+
+    InputManager::m_WatchedKeysElapsedTime[keyCode] =  1.0f;
+    InputManager::m_WatchedKeysDown[keyCode] =  false;
+    InputManager::m_WatchedKeysUp[keyCode] =  false;
+}
+
 bool InputManager::getKey(int keyCode) {
     return (glfwGetKey(InputManager::getWindowHandle(), keyCode) == GLFW_PRESS);
 }
 
 bool InputManager::getKeyDown(int keyCode) {
-    return false;
+    return InputManager::m_WatchedKeysDown[keyCode];
+}
+
+bool InputManager::getKeyUp(int keyCode) {
+    return InputManager::m_WatchedKeysUp[keyCode];
 }
 
 void InputManager::getMousePosition(Vector2d &mousePos) {
